b9d2135048e2cecd160387537dfcc43cb8e34e2f
Emirhan Bülbül <emirrhanbulbul@gmail.com>
2022-10-22 17:12:52 +0300

    view binding implementation
    view binding implementation

---
+++ app/src/main/java/com/emirhanbb/helloandroid/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.emirhanbb.helloandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emirhanbb.helloandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.hello.text = "Hello Android"
+    }
+}
